469348d282f5ab39cea2dc35ac09912519aeaae0
Lucas Ng <lucasng75@gmail.com>
2021-10-07 03:12:11 -0400

    tetris commit 4
    move left, move right, and rotate functions

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -62,6 +62,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   timerId = setInterval(moveDown, 1000);
 
+  function control(e) {
+      if(e.keyCode === 37) {
+        moveLeft()
+      } else if(e.keyCode === 38) {
+        rotate()
+      } else if(e.keyCode === 39) {
+        moveRight()
+      } else if(e.keyCode === 40) {
+        moveDown()
+      }
+  }
+  document.addEventListener('keyup', control)
+
   function moveDown() {
     undraw();
     currentPosition += width;
@@ -89,5 +102,27 @@ document.addEventListener('DOMContentLoaded', () => {
         currentPosition += 1
     }
     draw()
+  }
+
+  function moveRight() {
+      undraw()
+      const isAtRightEdge = current.some(index => (currentPosition + index)%width === width -1)
+
+      if(!isAtRightEdge) currentPosition += 1
+
+      if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition -= 1
+      }
+      draw()
+  }
+
+  function rotate() {
+      undraw()
+      currentRotation ++
+      if(currentRotation === current.length) {
+          currentRotation = 0
+      }
+      current = theTetrominoes[random][currentRotation]
+      draw()
   }
 });
