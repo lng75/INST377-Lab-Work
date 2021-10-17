49f1a5708f2cdc5a6d7e5242f178e7035a7322b3
Lucas Ng <lucasng75@gmail.com>
2021-10-17 16:13:34 -0400

    lab 7 commit 3
    final commit: changed find matches to use === instead of regex, and adjusted to include 5 matches, as well as checking for each coordinate to exist

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -4,6 +4,7 @@ async function windowActions() {
   const pgcounty = await request.json();
   const mymap = L.map('mapid').setView([38.990, -76.93], 12);
   const ACCESSTOKEN = 'pk.eyJ1IjoibHVjYXNuZzc1IiwiYSI6ImNrdXZkZzIxcDFrcGQybnE5eWc1ZjBnczkifQ.Ezajbr4gV6WgMSJKPY8gzg';
+  const markers = [];
 
   function mapInit() {
     L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${ACCESSTOKEN}`, {
@@ -17,15 +18,17 @@ async function windowActions() {
   }
 
   // filter by zip
-  function findMatches(wordToMatch, pgcounty) {
+  function findMatches(zipcode, pgcounty) {
     return pgcounty.filter((establishment) => {
-      const regex = new RegExp(wordToMatch, 'gi');
-      return establishment.zip.match(regex);
+      if (establishment.geocoded_column_1 != null) {
+        return establishment.zip === zipcode;
+      }
     });
   }
 
   function displayMatches(event) {
-    const matchArray = findMatches(event.target.value, pgcounty);
+    const matchArray = findMatches(event.target.value, pgcounty).slice(0, 5);
+
     const html = matchArray.map((place) => {
       const regex = new RegExp(this.value, 'gi');
       return `
@@ -40,14 +43,27 @@ async function windowActions() {
         </ul>
         <br>`;
     }).join('');
-    suggestions.innerHTML = html;
+    if (event.target.value === '') {
+      suggestions.innerHTML = '';
+    } else {
+      suggestions.innerHTML = html;
+      markers.forEach((marker) => {
+        marker.remove();
+      });
+      matchArray.forEach((item) => {
+        const point = item.geocoded_column_1;
+        const latLong = point.coordinates;
+        const marker = latLong.reverse();
+        markers.push(L.marker(marker).addTo(mymap));
+        // console.log(point)
+      });
+    }
   }
 
   const searchInput = document.querySelector('.search');
   const suggestions = document.querySelector('.suggestions');
 
-  searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', (evt) => {
+  searchInput.addEventListener('input', (evt) => {
     displayMatches(evt);
   });
   mapInit();
